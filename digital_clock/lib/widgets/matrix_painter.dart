1a561fcb301c1d1dd86d9bf9ab463ce90de93a3c
Nan YE <dailystudio2010@gmail.com>
2019-12-21 01:21:41 +0800

    [digital_clock][DEV]: add scale parameter to the text stream;

--- digital_clock/lib/widgets/matrix_painter.dart
+++ digital_clock/lib/widgets/matrix_painter.dart
@@ -4,37 +4,109 @@ import 'package:digital_clock/development/logger.dart';
 import 'package:flutter/material.dart';
 
 const CHARSET = "abcdefghijklmnopqrstuvwxyz0123456789";
-const MAX_CHARACTERS = 36;
-const MIN_CHARACTERS = 10;
+const MAX_CHARACTERS = 40;
+const MIN_CHARACTERS = 16;
 const MAX_SPEED = 6;
+
+const TEXT_STYLE = TextStyle(
+    color: Colors.green,
+    fontSize: 20,
+    height: 1
+);
+
+final _randomSeed = Random(DateTime.now().millisecondsSinceEpoch);
 
 class TextStream {
 
-  TextPainter painter;
-  Offset baseOffset;
-
+  final String text;
+  int charsCount;
   Size size;
+
+  Offset baseOffset;
   int yOffset = 0;
+
+  double baseScale = 1.0;
+  double scaleDelta = 0;
+
   int speed = 1;
 
-  TextStream(TextPainter painter, Offset baseOffset, Size size, int speed) {
-    this.painter = painter;
-    this.size = size;
-    this.baseOffset = baseOffset;
-    this.speed = speed;
+  TextPainter textPainter;
+
+  TextStream(this.text, Size boundary) {
+    _configureStreamParameters(boundary);
+  }
+
+  void _configureStreamParameters(Size boundary) {
+    this.charsCount = text.length;
+
+    this.baseScale = 0.5 + _randomSeed.nextDouble() * 0.5;
+
+    this.size = _measureChars(text, this.baseScale);
+
+    this.baseOffset = Offset(
+        _randomSeed.nextInt(boundary.width.round()).toDouble(),
+        -size.height * (_randomSeed.nextDouble() + 1)
+    );
+
+    this.speed = 1 + _randomSeed.nextInt(MAX_SPEED);
+    this.textPainter = createPainter();
+  }
+
+  TextPainter createPainter() {
+    final textStyle = TextStyle(
+        color: Colors.green,
+        fontSize: 20 * baseScale,
+        height: 1
+    );
+
+    final textSpan = TextSpan(
+      text: text,
+      style: textStyle,
+    );
+
+
+    TextPainter painter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
+
+    painter.layout(
+      minWidth: 0,
+      maxWidth: 0,
+    );
+
+    return painter;
+  }
+
+  static Size _measureChars(String chars, double scale) {
+    final textStyle = TextStyle(
+        color: Colors.green,
+        fontSize: 20 * scale,
+        height: 1
+    );
+
+    final singleCharTextSpan = TextSpan(
+      text: chars,
+      style: textStyle,
+    );
+
+    final painter = TextPainter(
+        text: singleCharTextSpan,
+        textDirection:  TextDirection.ltr,
+        textAlign: TextAlign.center
+    );
+
+    painter.layout(
+      minWidth: 0,
+      maxWidth: 0,
+    );
+
+    return Size(painter.minIntrinsicWidth, painter.height);
   }
 
 }
 
 class MatrixPainter extends CustomPainter {
-
-  static final _textStyle = TextStyle(
-      color: Colors.green,
-      fontSize: 20,
-      height: 1
-  );
-
-  static final _randomSeed = Random(DateTime.now().millisecondsSinceEpoch);
 
   double _position = 0;
   static int _loop = 0;
@@ -66,17 +138,19 @@ class MatrixPainter extends CustomPainter {
       final dy = stream.baseOffset.dy + stream.yOffset;
 
       stream.yOffset += stream.speed;
+      stream.scaleDelta += 0.001;
 
       if (dy < -stream.size.height || dy  > size.height) {
         if (dy > size.height) {
           useless.add(stream);
         }
+
         continue;
       }
 
       final offset = Offset(dx, dy);
 
-      stream.painter.paint(canvas, offset);
+      stream.textPainter.paint(canvas, offset);
     }
 
     for (TextStream s in useless) {
@@ -84,10 +158,27 @@ class MatrixPainter extends CustomPainter {
     }
   }
 
+  String _randomPickup(String old) {
+    String newString = old;
+    for (int i = 0; i < 3; i++) {
+      int position = _randomSeed.nextInt(old.length);
+      int index = _randomSeed.nextInt(CHARSET.length);
+      String char = CHARSET[index];
+
+      newString = _replaceCharAt(newString, position, char);
+    }
+    
+    return newString;
+  }
+
+  String _replaceCharAt(String oldString, int index, String newChar) {
+    return oldString.substring(0, index) + newChar + oldString.substring(index + 1);
+  }
+
   Size _measureChars(String chars) {
     final singleCharTextSpan = TextSpan(
       text: chars,
-      style: _textStyle,
+      style: TEXT_STYLE,
     );
 
     final painter = TextPainter(
@@ -104,59 +195,23 @@ class MatrixPainter extends CustomPainter {
     return Size(painter.minIntrinsicWidth, painter.height);
   }
 
-  Size _measureCharsAverage(String chars) {
-    final totalSize = _measureChars(chars);
-
-    return Size(totalSize.width,
-        totalSize.height / chars.length);
-  }
-
   String _randomString(int len) {
     String result = "";
 
     for (int i = 0; i < len; i++) {
       int index = _randomSeed.nextInt(CHARSET.length);
 
-        result += CHARSET[index];
+      result += CHARSET[index];
     }
 
     return result;
   }
 
   void _generateNewStream(Size boundary) {
-
     int len = MIN_CHARACTERS + _randomSeed.nextInt(MAX_CHARACTERS - MIN_CHARACTERS);
-    String text = _randomString(len);
-//    String text = CHARSET;
-    Size size = _measureChars(text);
-//    Size size = Size(17, CHARSET.length * 35.0);
-//    Logger.debug('new stream: [$text], size: $size');
 
-    final textSpan = TextSpan(
-      text: text,
-      style: _textStyle,
-    );
-
-    final textPainter = TextPainter(
-      text: textSpan,
-      textDirection: TextDirection.ltr,
-    );
-
-    textPainter.layout(
-        minWidth: 0,
-        maxWidth: 0,
-    );
-
-    Offset offset = Offset(
-      _randomSeed.nextInt(boundary.width.round()).toDouble(),
-      -size.height * (_randomSeed.nextDouble() + 1)
-    );
-
-    int speed = 1 + _randomSeed.nextInt(MAX_SPEED);
-
-    _textStreams.add(TextStream(textPainter, offset, size, speed));
+    _textStreams.add(TextStream(_randomString(len), boundary));
   }
-
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
