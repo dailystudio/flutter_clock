d037c44f981e900e974d4556395d8c065f0adcbf
Nan YE <dailystudio2010@gmail.com>
2020-01-10 17:52:00 +0800

    [digital_clock][ASSET]: update background image;

--- digital_clock/lib/digital_clock.dart
+++ digital_clock/lib/digital_clock.dart
@@ -121,9 +121,12 @@ class _DigitalClockState extends State<DigitalClock> {
           ),
           Positioned(  //
             left: 0, right: 0, top: 0, bottom: 0,
-            child: Image(
-              image: AssetImage('assets/images/background.jpg'),
-              fit : BoxFit.cover,
+            child: Opacity(
+              opacity: .2,
+              child: Image(
+                image: AssetImage('assets/images/background.jpg'),
+                fit : BoxFit.cover,
+              ),
             ),
           ),
           Positioned(  //
